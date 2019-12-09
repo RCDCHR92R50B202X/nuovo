12cf436113dac24ac08f8cce5a97eacdae219da5
RCDCHR92R50B202X <chiara_arcidiacono@yahoo.it>
2019-12-09 14:48:18 +0100

    first commit

---
+++ itpaperino/src/main/java/on/itpaperino/App.java
@@ -0,0 +1,13 @@
+package on.itpaperino;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
